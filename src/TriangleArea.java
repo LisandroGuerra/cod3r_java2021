e597590c7f0e4850781d51ec287329d5a360c36d
Lisandro Guerra <lisandro.digital@gmail.com>
2021-12-23 20:40:23 -0300

    Calculates the triangle area

--- src/TriangleArea.java
+++ src/TriangleArea.java
@@ -10,7 +10,7 @@ public class TriangleArea {
         System.out.print("Entry height value: ");
         double height = entry.nextDouble();
 
-        // Logic Area = b * h / 2
+        // Logic -> Area = b * h / 2
 
         System.out.printf("The triangle with base %.2f and height %.2f, has an area of %.2f.", base, height, base*height/2);
     }
